9ce4abc9b1c19a417155331a9193781fb8fd729f
Nabin Ojha <nabinojha47@gmail.com>
2025-05-28 16:36:33 +0545

    chore: sentry dsn to env file

--- src/sentry.ts
+++ src/sentry.ts
@@ -1,7 +1,7 @@
 import * as Sentry from '@sentry/react';
 
 Sentry.init({
-  dsn: 'https://e4669b965a4dc9a15998a67b154dca31@o4509394137448448.ingest.de.sentry.io/4509394139021392',
-  environment: process.env.NODE_ENV,
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  environment: import.meta.env.MODE,
   sendDefaultPii: true,
 });
